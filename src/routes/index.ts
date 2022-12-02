7ec356da820785789bd106b84227c94440faaf5d
lifeike <lifeike@gmail.com>
2022-12-02 02:05:17 -0700

    re

---
+++ src/routes/index.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+import AuthRouter from "./auth.route";
+
+// Init router
+const router = Router();
+
+/**
+ * Child Routes
+ */
+router.use("/auth", AuthRouter);
+
+/**
+ * Default handler - mainly for testing
+ */
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "I am on the line!" });
+});
+
+router.post("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Posted successfully", data: req.body, version: 1 });
+});
+
+/**
+ * Export base-router
+ */
+export default router;
---
+++ src/test/index.ts
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import { agent } from "supertest";
+import server from "../server";
+
+const request = agent;
+
+describe("Basic GET Request", () => {
+  it(`Get request to / returns JSON with message key: "I am on the line!"`, async () => {
+    const res = await request(server).get("/api");
+    const textResponse = res.body;
+    expect(res.status).to.equal(200);
+    expect(textResponse.message).to.be.a("string");
+    expect(textResponse.message).to.equal("I am on the line!");
+  });
+});
+
+describe("Basic POST Request", () => {
+  it(`Post request to / returns JSON with message and data (post data)`, async () => {
+    const res = await request(server).post("/api");
+    const textResponse = res.body;
+    expect(res.status).to.equal(200);
+    expect(textResponse.message).to.be.a("string");
+    expect(textResponse.message).to.equal("Posted successfully");
+    expect(textResponse.data).to.be.an("object");
+  });
+});
