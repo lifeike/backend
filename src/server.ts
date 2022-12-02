3168b6b19c82f55eea288fce6238f07238b4172a
lifeike <lifeike@gmail.com>
2022-12-02 02:09:41 -0700

    add db

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,33 @@
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import fs from "fs";
+import { db, collection } from "./db"
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import fs from "fs"
 
-import BaseRouter from "./routes";
+import BaseRouter from "./routes"
 
 /**
  * Init Express
  */
-const app = express();
+const app = express()
 
 /**
  * Add middleware/settings/routes to express.
  */
-app.use(cors()); // for testing only - make more specific for production app
-app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api", BaseRouter); // API Endpoint
+app.use(cors()) // for testing only - make more specific for production app
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use("/api", BaseRouter) // API Endpoint
 
 /**
  * Default root response
  */
 app.use("/", (req, res) => {
-  res.sendStatus(200);
-});
+  res.sendStatus(200)
+})
 
 /**
  * Export Express instance
  */
-export default app;
+export default app
