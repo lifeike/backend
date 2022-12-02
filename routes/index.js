43709c89c62e9ac87ba5da9b4e601b4fb0b82cde
lifeike <lifeike@gmail.com>
2022-12-02 01:44:17 -0700

    remove db

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const path = require("path")
 const { db, collection } = require("../db")
 
 router.get("/", async function (req, res) {
-  const findResult = await db.collection("documents").find({}).toArray()
+  //const findResult = await db.collection("documents").find({}).toArray()
   // console.log("Found documents =>", findResult)
-  res.send(findResult)
+  res.send("ok")
 })
 
 module.exports = router
