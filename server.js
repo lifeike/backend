41502ea692deab7f115b5a5abafc59a62b78f4b1
lifeike <lifeike@gmail.com>
2022-12-04 19:00:48 -0700

    remove cors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 var bodyParser = require("body-parser")
 
 const app = express()
-app.use(cors())
+//app.use(cors())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
