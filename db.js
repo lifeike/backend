7e17002a3b91a6e48a40322fa972ca4ba48c0229
lifeike <lifeike@gmail.com>
2022-12-06 16:05:02 -0700

    ad

--- db.js
+++ db.js
@@ -1,8 +1,4 @@
 const { MongoClient } = require("mongodb")
-// or as an es module:
-// import { MongoClient } from 'mongodb'
-
-// Connection URL
 const url = "mongodb+srv://feeco:Test%401234@cluster0.de2gw.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(url)
 client.connect()
