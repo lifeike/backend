b9aa3a9c1978b67476a52a0a7f5be2e243c79171
lifeike <lifeike@gmail.com>
2022-12-06 11:54:18 -0700

    ad

--- db.js
+++ db.js
@@ -5,13 +5,10 @@ const { MongoClient } = require("mongodb")
 // Connection URL
 const url = "mongodb+srv://feeco:Test%401234@cluster0.de2gw.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(url)
-
-// Database Name
-const dbName = "myProject"
-
 client.connect()
 console.log("Connected successfully to server")
-const db = client.db(dbName)
-const collection = db.collection("documents")
+
+const db = client.db("template")
+const collection = db.collection("users")
 
 module.exports = { db, collection }
