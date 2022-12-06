55aafa78ff11663ab9aea963c34224872d37802b
lifeike <lifeike@gmail.com>
2022-12-06 16:24:09 -0700

    ad

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const path = require("path")
 const db = require("../db")
 
 router.get("/", async function (req, res) {
-  const findResult = await db.collection("users").find({}).toArray()
+  const findResult = await db.collection("movie").find({}).toArray()
   // console.log("Found documents =>", findResult)
   res.send(findResult)
 })
