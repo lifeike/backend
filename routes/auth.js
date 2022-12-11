0d52f4037d376aac70d015b991d74bff8340ebc1
lifeike <lifeike@gmail.com>
2022-12-11 13:57:49 -0700

    ad

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ const path = require("path")
 const db = require("../db")
 
 router.post("/signIn", async function (req, res) {
+  console.log(req.body)
+
   let payload = {
     iss: "gusibi.mobi",
     expiresIn: "200",
@@ -12,9 +14,13 @@ router.post("/signIn", async function (req, res) {
     aud: "www.test.com",
     issuer: "feeco",
   }
-  let token = jwt.sign(payload, "secret", { expiresIn: "200" })
-  let refreshToken = jwt.sign({ ...payload, type: "refresh-token" }, "secret", { expiresIn: "8h" })
-  res.send({ token, refreshToken, user: { name: "feeco", age: 30 } })
+  let access_token = jwt.sign(payload, "secret", { expiresIn: "200" })
+  let refresh_token = jwt.sign({ ...payload, type: "refresh-token" }, "secret", { expiresIn: "8h" })
+  if (req.body.username == "feeco" && req.body.password == "li") {
+    res.send({ access_token, refresh_token, user: { name: "feeco", age: 30 } })
+  } else {
+    res.status(401).send("invalid credentials.")
+  }
 })
 
 module.exports = router
