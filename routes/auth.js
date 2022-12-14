76e92bd4fcc6648a345240ec870f7a49b76d544d
lifeike <lifeike@gmail.com>
2022-12-14 15:34:34 -0700

    ad

--- routes/auth.js
+++ routes/auth.js
@@ -39,6 +39,8 @@ router.post("/refresh-token", async function (req, res) {
   }
 
   try {
+    //verify refresh token first
+    const decoded = jwt.verify(refresh_token, "secret")
     let access_token = jwt.sign(access_token_payload, "secret")
     res.send({ access_token, refresh_token, user: { name: "feeco", age: 30 } })
     // throw new Error("hello world")
