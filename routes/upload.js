cf84ad0d0f0a043fcb3da5ae818f6a95e776c5e0
lifeike <lifeike@gmail.com>
2022-12-14 22:33:15 -0700

    ad

--- routes/upload.js
+++ routes/upload.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const path = require("path")
 const verify = require("../middlewares/authVerify")
 const db = require("../db")
+const uploadToS3 = require("../helpers/uploadToS3")
+const { v4: uuidv4 } = require("uuid")
 
 router.post("/image", verify, async (req, res) => {
   const { image } = req.body
-  const url = await uploadToS3(image, uuid())
+  const url = await uploadToS3(image, uuidv4())
   return res.json({ url })
 })
 
