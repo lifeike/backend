901fb89adb9d9f06fe2ba7e7049d9cd7ea5af4d9
lifeike <lifeike@gmail.com>
2022-12-15 03:31:08 -0700

    ad

--- routes/upload.js
+++ routes/upload.js
@@ -5,20 +5,34 @@ const verify = require("../middlewares/authVerify")
 const db = require("../db")
 const { v4: uuidv4 } = require("uuid")
 const multer = require("multer")
+const { S3Client } = require("@aws-sdk/client-s3")
+const multerS3 = require("multer-s3")
 
-router.post("/store-image-locally", multer({ dest: "./uploadImages/" }).array("uploaded-images"), async (req, res) => {
+const uploadLocally = multer({ dest: "./uploadImages/" })
+router.post("/store-image-locally", uploadLocally.array("uploaded-images"), async (req, res) => {
   //in backend source code folder ./uploadImages/
   console.log(req.files)
   console.log(req.body)
   res.send("ok")
 })
 
-router.post("/store-image-aws-s3", multer({ dest: "./uploadImages/" }).array("uploaded-images"), async (req, res) => {
-  // const { image } = req.body
-  // const url = await uploadToS3(image, uuidv4())
-  // return res.json({ url })
-  console.log(req.files)
-  console.log(req.body)
+const s3 = new S3Client()
+const uploadToS3 = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "some-bucket",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname })
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    },
+  }),
+})
+
+router.post("/store-image-aws-s3", uploadToS3.array("uploaded-images"), async (req, res) => {
+  console.log(res.files)
+  console.log(res.files.location)
   res.send("ok")
 })
 
