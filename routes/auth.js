91a59973f31bfbc1ad2a6ad094db751321de1895
lifeike <lifeike@gmail.com>
2022-12-19 22:21:56 -0700

    ad

--- routes/auth.js
+++ routes/auth.js
@@ -46,9 +46,6 @@ router.post("/refresh-token", async function (req, res) {
   try {
     //verify refresh token first, if no error, continue
     const decoded = jwt.verify(refresh_token, "secret")
-    console.log("test")
-    console.log(decoded)
-    console.log("test")
     let user = await db.collection("users").findOne({ email: decoded.email })
     let access_token = jwt.sign(access_token_payload, "secret")
     res.send({ access_token, refresh_token, user })
