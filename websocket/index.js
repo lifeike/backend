a24a5d188c641cf9d168610e4bfa976d1f4d184f
lifeike <lifeike@gmail.com>
2022-12-26 05:43:35 -0700

    ad

--- websocket/index.js
+++ websocket/index.js
@@ -1,17 +1,35 @@
-let id = 0
-let clients = {}
+const rooms = {}
 
-const websocket = async (webSocketServer) => {
-  webSocketServer.on("connection", function (websocket, request) {
-    console.log("started client websocket")
-    let connectionId = request.headers["sec-websocket-key"]
+const websocket = async (wss) => {
+  wss.on("connection", (socket) => {
+    const uuid = "test" // create here a uuid for this connection
+    const leave = (room) => {
+      // not present: do nothing
+      if (!rooms[room][uuid]) return
 
-    websocket.on("message", function (data) {
-      console.log(data)
+      // if the one exiting is the last one, destroy the room
+      if (Object.keys(rooms[room]).length === 1) delete rooms[room]
+      // otherwise simply leave the room
+      else delete rooms[room][uuid]
+    }
+
+    socket.on("message", (data) => {
+      const { message, meta, room } = data
+
+      if (meta === "join") {
+        if (!rooms[room]) rooms[room] = {} // create the room
+        if (!rooms[room][uuid]) rooms[room][uuid] = socket // join the room
+      } else if (meta === "leave") {
+        leave(room)
+      } else if (!meta) {
+        // send the message to all in the room
+        Object.entries(rooms[room]).forEach(([, sock]) => sock.send({ message }))
+      }
     })
 
-    websocket.on("close", function () {
-      console.log("stopping client websocket")
+    socket.on("close", () => {
+      // for each room, remove the closed socket
+      Object.keys(rooms).forEach((room) => leave(room))
     })
   })
 }
