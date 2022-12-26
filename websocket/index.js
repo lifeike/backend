7a5f838a46892f1451c9c36c4679596175f38b7d
lifeike <lifeike@gmail.com>
2022-12-26 05:44:58 -0700

    ad

--- websocket/index.js
+++ websocket/index.js
@@ -1,21 +1,13 @@
+const { v4: uuidv4 } = require("uuid")
+
 const rooms = {}
 
 const websocket = async (wss) => {
   wss.on("connection", (socket) => {
-    const uuid = "test" // create here a uuid for this connection
-    const leave = (room) => {
-      // not present: do nothing
-      if (!rooms[room][uuid]) return
-
-      // if the one exiting is the last one, destroy the room
-      if (Object.keys(rooms[room]).length === 1) delete rooms[room]
-      // otherwise simply leave the room
-      else delete rooms[room][uuid]
-    }
+    const uuid = uuidv4() // create here a uuid for this connection
 
     socket.on("message", (data) => {
       const { message, meta, room } = data
-
       if (meta === "join") {
         if (!rooms[room]) rooms[room] = {} // create the room
         if (!rooms[room][uuid]) rooms[room][uuid] = socket // join the room
@@ -32,6 +24,16 @@ const websocket = async (wss) => {
       Object.keys(rooms).forEach((room) => leave(room))
     })
   })
+}
+
+const leave = (room) => {
+  // not present: do nothing
+  if (!rooms[room][uuid]) return
+
+  // if the one exiting is the last one, destroy the room
+  if (Object.keys(rooms[room]).length === 1) delete rooms[room]
+  // otherwise simply leave the room
+  else delete rooms[room][uuid]
 }
 
 module.exports = websocket
