2b2bf94027a574dfebc50a44e0c44d1d7d50494b
lifeike <lifeike@gmail.com>
2022-12-25 17:10:01 -0700

    ad

--- websocket/index.js
+++ websocket/index.js
@@ -1,7 +1,6 @@
 const websocket = async (wss) => {
   wss.on("connection", function (ws) {
     console.log("started client websocket")
-    console.log(ws)
 
     ws.on("message", function (data) {
       console.log("received: %s", data)
