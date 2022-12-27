2ebae6ef3fac691e0e648d4be935476eb13a3187
lifeike <lifeike@gmail.com>
2022-12-27 08:03:36 -0700

    ad

--- websocket/index.js
+++ websocket/index.js
@@ -1,13 +1,31 @@
 const { WebSocketServer } = require("ws")
-const messageHandler = require("./messageHandler")
+const { v4: uuidv4 } = require("uuid")
+const rooms = {}
 
 const realTimeChatWebsocket = async (expressSever) => {
   const wss = new WebSocketServer({ server: expressSever })
+  const uuid = uuidv4() // create here a uuid for this connection
 
   wss.on("connection", (socket) => {
     socket.on("message", async (data) => {
       const { meta, room, message } = JSON.parse(data)
-      messageHandler(meta, room, message, socket)
+      if (meta === "join") {
+        if (!rooms[room]) rooms[room] = {} // create the room
+        if (!rooms[room][uuid]) rooms[room][uuid] = socket // join the room
+        socket.send(JSON.stringify({ meta: "joined", room, message: `You have joined room ${room}` }))
+      } else if (meta === "leave") {
+        // not present: do nothing
+        if (rooms[room] == undefined) return
+        // if the one exiting is the last one, destroy the room
+        if (Object.keys(rooms[room]).length === 1) delete rooms[room]
+        // otherwise simply leave the room
+        else delete rooms[room][uuid]
+      } else if (meta === "message") {
+        // send the message to all in the room
+        for (const [key, sock] of Object.entries(rooms[room])) {
+          sock.send(JSON.stringify({ meta, room, message }))
+        }
+      }
     })
 
     socket.on("close", () => {
