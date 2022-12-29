8454f1a60201029bd892b80ef326bc6e99d7de29
lifeike <lifeike@gmail.com>
2022-12-28 20:34:37 -0700

    ad

---
+++ aws/s3.js
@@ -0,0 +1,13 @@
+const { S3Client } = require("@aws-sdk/client-s3")
+let s3 = new S3Client({
+  region: "ca-central-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+  sslEnabled: false,
+  s3ForcePathStyle: true,
+  signatureVersion: "v4",
+})
+
+module.exports = s3
--- routes/upload.js
+++ routes/upload.js
@@ -5,8 +5,8 @@ const verify = require("../middlewares/authVerify")
 const db = require("../db")
 const { v4: uuidv4 } = require("uuid")
 const multer = require("multer")
-const { S3Client } = require("@aws-sdk/client-s3")
 const multerS3 = require("multer-s3")
+const s3 = require("../aws/s3")
 
 //1. in backend source code folder ./uploadImages/
 const uploadLocally = multer({ dest: "./uploadImages/" })
@@ -17,27 +17,15 @@ router.post("/store-image-locally", uploadLocally.array("uploaded-images"), asyn
 })
 
 //2. uploaded files location is url,  store url mongo atlas
-let s3 = new S3Client({
-  region: "ca-central-1",
-  credentials: {
-    accessKeyId: "AKIARBXBS625SO2IKA57",
-    secretAccessKey: "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",
-  },
-  sslEnabled: false,
-  s3ForcePathStyle: true,
-  signatureVersion: "v4",
-})
 const uploadToS3 = multer({
   storage: multerS3({
     s3: s3,
     bucket: "feeco-backend-bucket",
     contentType: multerS3.AUTO_CONTENT_TYPE, //important !!
     metadata: function (req, file, cb) {
-      console.log(file.size)
       cb(null, { fieldName: file.fieldname })
     },
     key: function (req, file, cb) {
-      console.log(file.size)
       cb(null, Date.now().toString())
     },
   }),
