43e53bf1a18a13ce49f7a6d8e3372e467dd20577
lifeike <lifeike@gmail.com>
2023-01-07 22:31:42 -0700

    ad

--- aws/s3.js
+++ aws/s3.js
@@ -9,10 +9,10 @@ let credentials
 
 let s3 = new S3Client({
   region: "ca-central-1",
-  credentials: {
-    accessKeyId: credentials.AWS_ACCESS_KEY,
-    secretAccessKey: credentials.AWS_SECRET_KEY,
-  },
+  // credentials: {
+  //   accessKeyId: credentials.AWS_ACCESS_KEY,
+  //   secretAccessKey: credentials.AWS_SECRET_KEY,
+  // },
   sslEnabled: false,
   s3ForcePathStyle: true,
   signatureVersion: "v4",
