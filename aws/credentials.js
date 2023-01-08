35b56510b6775f0868114953d3799cd5a7b79e73
lifeike <lifeike@gmail.com>
2023-01-07 23:37:07 -0700

    ad

--- aws/credentials.js
+++ aws/credentials.js
@@ -1,7 +1,2 @@
 const axios = require("axios")
-
-const getCredentials = async () => {
-  await axios.get("http://169.254.169.254/latest/meta-data/iam/security-credentials/ec2-instance-full-access-s3-feeco-created-role")
-}
-
-module.exports = getCredentials
+module.exports = axios.get("http://169.254.169.254/latest/meta-data/iam/security-credentials/ec2-instance-full-access-s3-feeco-created-role")
--- aws/s3.js
+++ aws/s3.js
@@ -1,15 +1,19 @@
 const { S3Client } = require("@aws-sdk/client-s3")
-const credentials = require("./credentials")
+const credentialsPromise = require("./credentials")
 
-let s3 = new S3Client({
-  region: "ca-central-1",
-  credentials: {
-    accessKeyId: credentials.AWS_ACCESS_KEY,
-    secretAccessKey: credentials.AWS_SECRET_KEY,
-  },
-  sslEnabled: false,
-  s3ForcePathStyle: true,
-  signatureVersion: "v4",
+let s3
+
+credentialsPromise.then((credentials) => {
+  s3 = new S3Client({
+    region: "ca-central-1",
+    credentials: {
+      accessKeyId: credentials.AWS_ACCESS_KEY,
+      secretAccessKey: credentials.AWS_SECRET_KEY,
+    },
+    sslEnabled: false,
+    s3ForcePathStyle: true,
+    signatureVersion: "v4",
+  })
 })
 
 module.exports = s3
