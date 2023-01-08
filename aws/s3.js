0c99ea5ecfc19e0a8c4f719ace6692b68aaf20d7
lifeike <lifeike@gmail.com>
2023-01-07 21:55:37 -0700

    ad

--- aws/s3.js
+++ aws/s3.js
@@ -1,6 +1,6 @@
 const { S3Client } = require("@aws-sdk/client-s3")
 let get = require("./credentials")
-console.log(get)
+get.then((res) => console.log(res))
 
 let s3 = new S3Client({
   region: "ca-central-1",
