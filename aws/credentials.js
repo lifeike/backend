921d7449062a308a1e3d4b42dce281ecf23bd4ec
lifeike <lifeike@gmail.com>
2023-01-08 05:23:50 -0700

    ad

--- aws/credentials.js
+++ aws/credentials.js
@@ -1,2 +1,4 @@
 const axios = require("axios")
-module.exports = axios.get("http://169.254.169.254/latest/meta-data/iam/security-credentials/ec2-instance-full-access-s3-feeco-created-role")
+module.exports = axios.get("http://169.254.169.254/latest/meta-data/iam/security-credentials/ec2-instance-full-access-s3-feeco-created-role").then((res) => {
+  return () => res.data
+})
