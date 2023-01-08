1c41d46dcd6d6ef3cf628e467055a168d39d1242
lifeike <lifeike@gmail.com>
2023-01-07 22:23:09 -0700

    ad

--- aws/credentials.js
+++ aws/credentials.js
@@ -3,7 +3,9 @@ const axios = require("axios")
 let result
 const getCredentials = async () => {
   result = await axios.get("http://169.254.169.254/latest/meta-data/iam/security-credentials/ec2-instance-full-access-s3-feeco-created-role")
+  console.log("test")
   console.log(result)
+  console.log("test")
   return result
 }
 getCredentials()
