2aaeb4896651dbc5a0a6f564b8ba71821a528032
lifeike <lifeike@gmail.com>
2023-01-08 04:51:12 -0700

    ad

--- aws/s3.js
+++ aws/s3.js
@@ -2,15 +2,17 @@ const { S3Client } = require("@aws-sdk/client-s3")
 const credentials = require("./credentials")
 console.log(credentials)
 
-let s3 = new S3Client({
-  region: "ca-central-1",
-  credentials: {
-    accessKeyId: credentials.AWS_ACCESS_KEY,
-    secretAccessKey: credentials.AWS_SECRET_KEY,
-  },
-  sslEnabled: false,
-  s3ForcePathStyle: true,
-  signatureVersion: "v4",
-})
+credentials.then((res) => {
+  let s3 = new S3Client({
+    region: "ca-central-1",
+    credentials: {
+      accessKeyId: res.data.AWS_ACCESS_KEY,
+      secretAccessKey: res.data.AWS_SECRET_KEY,
+    },
+    sslEnabled: false,
+    s3ForcePathStyle: true,
+    signatureVersion: "v4",
+  })
 
-module.exports = s3
+  module.exports = s3
+})
