bee064f9aaf60916c0a2f352cc3e29a6a288e400
lifeike <lifeike@gmail.com>
2023-01-07 22:08:25 -0700

    ad

--- aws/s3.js
+++ aws/s3.js
@@ -1,7 +1,7 @@
 const { S3Client } = require("@aws-sdk/client-s3")
 const result = require("./credentials")
 console.log("test")
-console.log(result.data)
+console.log(result?.data)
 console.log("test")
 
 let s3 = new S3Client({
