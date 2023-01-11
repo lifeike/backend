6a754c8b3da90d9549892a5a0a2df3f08a0a5b28
lifeike <lifeike@gmail.com>
2023-01-11 10:50:53 -0700

    ad

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const app = express()
-app.use(cors({ origin: "https://main.d3nhqx7mts8be0.amplifyapp.com/" })) //enable production url
+// app.use(cors({ origin: "https://main.d3nhqx7mts8be0.amplifyapp.com/" })) //enable production url
+app.use(cors()) //enable production url
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
