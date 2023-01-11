999fde32f3d70c9baafb9e9c5b00712128fe1b25
lifeike <lifeike@gmail.com>
2023-01-11 09:50:51 -0700

    ad

--- aws/credentials.js
+++ aws/credentials.js
@@ -3,7 +3,7 @@ let result
 
 try {
   result = request("GET", "http://169.254.169.254/latest/meta-data/iam/security-credentials/ec2-instance-full-access-s3-feeco-created-role")
-  console.log(result.getBody())
+  console.log(result.getBody("utf-8"))
 } catch (e) {}
 
 module.exports = result
