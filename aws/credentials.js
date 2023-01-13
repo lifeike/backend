94e6b9abd77df5fa82cdaad924634d6b474b96ba
lifeike <lifeike@gmail.com>
2023-01-13 05:32:38 -0700

    ad

--- aws/credentials.js
+++ aws/credentials.js
@@ -1,8 +1,12 @@
-const request = require("sync-request")
-let result
+// const request = require("sync-request")
+// let result
 
-try {
-  result = request("GET", "http://169.254.169.254/latest/meta-data/iam/security-credentials/ec2-instance-full-access-s3-feeco-created-role")
-} catch (e) {}
+// try {
+//   result = request("GET", "http://169.254.169.254/latest/meta-data/iam/security-credentials/ec2-instance-full-access-s3-feeco-created-role")
+// } catch (e) {}
 
-module.exports = result.getBody("utf-8")
+// module.exports = result.getBody("utf-8")
+module.exports = {
+  AWS_ACCESS_KEY: "FJDKLSAJFKADJ",
+  AWS_SECRET_KEY: "FJADSIJFLKJ",
+}
