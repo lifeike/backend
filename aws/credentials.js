51e768c4bafbb03de9efe238c7ff8911557ecc8f
lifeike <lifeike@gmail.com>
2023-02-04 23:42:53 -0700

    ad

--- aws/credentials.js
+++ aws/credentials.js
@@ -6,6 +6,6 @@ const credentials = fs.readFileSync(`${homedir}/.aws-credentials`, "utf8").toStr
 console.log(credentials)
 
 module.exports = {
-  AWS_ACCESS_KEY: process.env.aws_access_key,
-  AWS_SECRET_KEY: process.env.aws_secret_access_key,
+  AWS_ACCESS_KEY: credentials[0],
+  AWS_SECRET_KEY: credentials[1],
 }
