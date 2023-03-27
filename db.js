35687a4f96aa90ad753435d1812c4940eccc07de
lifeike <lifeike@gmail.com>
2023-03-26 23:38:09 -0600

    fix

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const { MongoClient } = require("mongodb")
 const url = "mongodb+srv://feeco:Test%401234@cluster0.de2gw.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(url)
 client.connect()
-
 console.log("database running.")
 
 const db = client.db("template")
