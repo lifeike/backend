9d9ac7c115b2c8e6ae1d118b519874129ea50d3a
lifeike <lifeike@gmail.com>
2023-04-01 15:02:27 -0600

    ad

--- server.js
+++ server.js
@@ -7,19 +7,11 @@ dotenv.config()
 
 const app = express()
 // app.use(cors({ origin: "https://main.d3nhqx7mts8be0.amplifyapp.com/" })) //enable production url
-app.use(cors()) //enable production url
+app.use(cors({ origin: "*" })) //enable production url
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
 app.use("/", require("./routes/index"))
-app.use("/auth", require("./routes/auth"))
-app.use("/user", require("./routes/user"))
-app.use("/movie", require("./routes/movie"))
-app.use("/upload", require("./routes/upload"))
-app.use("/chat", require("./routes/chat"))
-
-require("./websocket/realTimeChat") //start real time websocket server 8081
-require("./websocket/goGame") //start go game websocket server 8082
 app.listen(8080, () => console.log("http server running on port 8080")) //http server 8080
