83dd9f990f6a8a439fbaf62def21ef3f767aa42b
lifeike <lifeike@gmail.com>
2023-06-08 23:48:23 -0600

    ad

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,11 +1,22 @@
 const db = require("@/config/db/mongoDB")
-const movieServices = require("@/services/movie")
 
 // In src/controllers/workoutController.js
 const getAllMovies = async (req, res) => {
   //pagination receive two params: items_per_page
-  const allMovies = movieServices.getAllMovies()
-  res.send(allMovies)
+  const total = await db.collection("movies").count()
+  if (req.query.items_per_page && req.query.page_number) {
+    const movieList = await db
+      .collection("movies")
+      .find({})
+      .skip(req.query.items_per_page * req.query.page_number)
+      .limit(+req.query.items_per_page)
+      .toArray()
+    res.send({ totalPages: total / req.query.items_per_page, movieList })
+  } else {
+    //or  if items_per_page is empty, return all items
+    const movieList = await db.collection("movies").find({}).toArray()
+    res.send({ totalPages: null, movieList })
+  }
 }
 
 const getOneMovie = async (req, res) => {
