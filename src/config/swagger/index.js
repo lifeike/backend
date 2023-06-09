a9c029d9fa5ccdcda80b31a42181f735f0768fd3
lifeike <lifeike@gmail.com>
2023-06-09 01:34:01 -0600

    fix

--- src/config/swagger/index.js
+++ src/config/swagger/index.js
@@ -6,9 +6,27 @@ const swaggerUi = require("swagger-ui-express")
 const options = {
   definition: {
     openapi: "3.0.0",
-    info: { title: "Feeco Backend API", version: "1.0.0" },
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description: "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
   },
-  apis: ["./src/routes/movie.js"],
+  apis: ["./routes/*.js"],
 }
 
 // Docs in JSON format
