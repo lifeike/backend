a4d413f3fc712f523c877898b6aec236481ff382
lifeike <lifeike@gmail.com>
2023-06-09 01:34:58 -0600

    ad

--- src/config/swagger/index.js
+++ src/config/swagger/index.js
@@ -7,7 +7,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "LogRocket Express API with Swagger",
+      title: "Feeco Express API with Swagger",
       version: "0.1.0",
       description: "This is a simple CRUD API application made with Express and documented with Swagger",
       license: {
@@ -26,7 +26,7 @@ const options = {
       },
     ],
   },
-  apis: ["./routes/*.js"],
+  apis: ["@/routes/movie"],
 }
 
 // Docs in JSON format
