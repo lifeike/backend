6eb6aac98b08961f311e2a03c738f3c05324cdd0
lifeike <lifeike@gmail.com>
2023-06-08 23:28:16 -0600

    ad

--- src/routes/movie.js
+++ src/routes/movie.js
@@ -7,7 +7,6 @@ const { ObjectID, ObjectId } = require("bson")
 
 //get all movies
 router.get("/", async function (req, res) {
-  console.log("test")
   //pagination receive two params: items_per_page
   const total = await db.collection("movies").count()
   if (req.query.items_per_page && req.query.page_number) {
