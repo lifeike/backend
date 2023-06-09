e4f4eb936117282fc86418d9a8586c2810d0cb00
lifeike <lifeike@gmail.com>
2023-06-09 00:55:06 -0600

    ad

--- src/routes/movie.js
+++ src/routes/movie.js
@@ -30,13 +30,97 @@ const { ObjectID, ObjectId } = require("bson")
  *                     type: object
  */
 router.get("/", movieController.getAllMovies)
-//get one movie
+/**
+ * @openapi
+ * /api/v1/moives/:id:
+ *   get:
+ *     tags:
+ *       - Movies
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
 router.get("/:id", verify, movieController.getOneMovie)
-//careate one movie
+/**
+ * @openapi
+ * /api/v1/moives:
+ *   post:
+ *     tags:
+ *       - Movies
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
 router.post("/", verify, movieController.createMovie)
-//upodate one movie
+/**
+ * @openapi
+ * /api/v1/moives/:id:
+ *   put:
+ *     tags:
+ *       - Movies
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
 router.put("/:id", verify, movieController.updateMovie)
-//delete one movie
+/**
+ * @openapi
+ * /api/v1/moives:
+ *   delete:
+ *     tags:
+ *       - Movies
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
 router.delete("/:id", verify, movieController.deleteMovie)
 
 module.exports = router
