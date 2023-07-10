1eb3d03e4ead7fa6273e829976009ac102ee1caf
lifeike <lifeike@gmail.com>
2023-07-09 23:32:34 -0600

    fix

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -4,36 +4,28 @@ const pick = require("@/utils/pick")
 const catchAsync = require("@/utils/catchAsync")
 
 // In src/controllers/workoutController.js
-const getAllMovies = catchAsync(async (req, res) => {
+exports.getAllMovies = catchAsync(async (req, res) => {
   //pagination receive two params: items_per_page
   const filter = pick(req.query, ["items_per_page", "page_number"])
   const allMovies = await movieServices.getAllMovies(filter)
   res.send({ status: "ok", data: allMovies })
 })
 
-const getOneMovie = async (req, res) => {
+exports.getOneMovie = async (req, res) => {
   const movie = await db.collection("movies").findOne({ _id: ObjectId(req.body.id) })
   res.send(movie)
 }
 
-const createMovie = async (req, res) => {
+exports.createMovie = async (req, res) => {
   res.send("Create a new movie")
 }
 
-const updateMovie = async (req, res) => {
+exports.updateMovie = async (req, res) => {
   const movie = await db.collection("movies").findOneAndUpdate({ _id: ObjectId(req.params.id) }, { $set: { ...req.body } })
   res.send(movie)
 }
 
-const deleteMovie = async (req, res) => {
+exports.deleteMovie = async (req, res) => {
   const movie = await db.collection("movies").findOneAndDelete({ _id: ObjectId(req.params.id) })
   res.send({ message: "You have successfully deleted this item." })
-}
-
-module.exports = {
-  getAllMovies,
-  getOneMovie,
-  createMovie,
-  updateMovie,
-  deleteMovie,
 }
--- src/validations/movie.js
+++ src/validations/movie.js
@@ -1,15 +1,3 @@
 const Joi = require("joi")
 
-const getAllMovies = {
-  query: Joi.object().keys({
-    name: Joi.string(),
-    role: Joi.string(),
-    sortBy: Joi.string(),
-    limit: Joi.number().integer(),
-    page: Joi.number().integer(),
-  }),
-}
-
-module.exports = {
-  getAllMovies,
-}
+exports.getAllMovies = {}
