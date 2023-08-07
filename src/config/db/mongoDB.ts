d69d3004c7dc32083ff0617be538c6a752f5d784
lifeike <lifeike@gmail.com>
2023-08-07 13:39:25 -0600

    ad

--- src/config/db/mongoDB.ts
+++ src/config/db/mongoDB.ts
@@ -1,4 +1,5 @@
-const { MongoClient } = require("mongodb")
+// const { MongoClient } = require("mongodb")
+import { MongoClient } from "mongodb"
 const url = "mongodb+srv://feeco:Test%401234@cluster0.de2gw.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(url)
 client.connect()
