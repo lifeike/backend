3500c87f7df73cdb62dec5403225b9d9130b55b9
lifeike <lifeike@gmail.com>
2023-08-08 01:25:07 -0600

    ad

--- src/models/movie.ts
+++ src/models/movie.ts
@@ -1,6 +1,6 @@
 import db from "@/config/db/mongoDB"
 
-const getAllMovies = async (filterParams: any) => {
+export const getAllMovies = async (filterParams: any) => {
   const total = await db.collection("movies").count()
   const currentPage = filterParams.page_number
   const movies = await db
@@ -10,8 +10,4 @@ const getAllMovies = async (filterParams: any) => {
     .limit(+filterParams.items_per_page)
     .toArray()
   return { totalPages: total / filterParams.items_per_page, movies, currentPage }
-}
-
-module.exports = {
-  getAllMovies,
 }
