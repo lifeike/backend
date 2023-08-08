512f36b4f7b0f52b15e35160f9a69c0c9b4d2326
lifeike <lifeike@gmail.com>
2023-08-08 17:45:19 -0600

    can't find module movie

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const path = require("path")
 
 router.get("/", (req, res) => res.send("Feeco's server is running."))
-router.use("/movies", require("./movie"))
+router.use("/movies", require("./movie.ts"))
 // router.use("/auth", require("./auth"))
 // router.use("/chat", require("./chat"))
 // router.use("/user", require("./user"))
