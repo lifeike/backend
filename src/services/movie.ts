ebc47d96b67a9913545f41c4577a4dd7925c8186
lifeike <lifeike@gmail.com>
2023-08-08 02:22:15 -0600

    fix

--- src/services/movie.ts
+++ src/services/movie.ts
@@ -2,34 +2,26 @@ import db from "@/config/db/mongoDB"
 import * as movieModel from "@/models/movie"
 
 // In src/controllers/workoutController.js
-const getAllMovies = async (filter: any) => {
+export const getAllMovies = async (filter: any) => {
   const movies = await movieModel.getAll(filter)
   return movies
 }
 
-const getOneMovie = async (req: any, res: any) => {
+export const getOneMovie = async (req: any, res: any) => {
   const movie = await db.collection("movies").findOne({ _id: req.body.id })
   res.send(movie)
 }
 
-const createMovie = async (req: any, res: any) => {
+export const createMovie = async (req: any, res: any) => {
   res.send("Create a new movie")
 }
 
-const updateMovie = async (req: any, res: any) => {
+export const updateMovie = async (req: any, res: any) => {
   const movie = await db.collection("movies").findOneAndUpdate({ _id: req.params.id }, { $set: { ...req.body } })
   res.send(movie)
 }
 
-const deleteMovie = async (req: any, res: any) => {
+export const deleteMovie = async (req: any, res: any) => {
   const movie = await db.collection("movies").findOneAndDelete({ _id: req.params.id })
   res.send({ message: "You have successfully deleted this item." })
-}
-
-module.exports = {
-  getAllMovies,
-  getOneMovie,
-  createMovie,
-  updateMovie,
-  deleteMovie,
 }
