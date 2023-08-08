16f0e2c8225576735d1b58297bd078fdf991f069
lifeike <lifeike@gmail.com>
2023-08-08 02:37:42 -0600

    fix

--- src/services/movie.ts
+++ src/services/movie.ts
@@ -7,21 +7,21 @@ export const getAll = async (filter: any) => {
   return movies
 }
 
-export const getOneMovie = async (req: any, res: any) => {
+export const getOne = async (req: any, res: any) => {
   const movie = await db.collection("movies").findOne({ _id: req.body.id })
   res.send(movie)
 }
 
-export const createMovie = async (req: any, res: any) => {
+export const createOne = async (req: any, res: any) => {
   res.send("Create a new movie")
 }
 
-export const updateMovie = async (req: any, res: any) => {
+export const updateOne = async (req: any, res: any) => {
   const movie = await db.collection("movies").findOneAndUpdate({ _id: req.params.id }, { $set: { ...req.body } })
   res.send(movie)
 }
 
-export const deleteMovie = async (req: any, res: any) => {
+export const deleteOne = async (req: any, res: any) => {
   const movie = await db.collection("movies").findOneAndDelete({ _id: req.params.id })
   res.send({ message: "You have successfully deleted this item." })
 }
