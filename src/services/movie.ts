1178c9c7a96ed9f7337c4ace76606a2900e12c54
lifeike <lifeike@gmail.com>
2023-08-08 15:08:49 -0600

    ad

--- src/services/movie.ts
+++ src/services/movie.ts
@@ -1,7 +1,6 @@
 import db from "@/config/db/mongoDB"
 import { ObjectId } from "mongodb"
 import * as movieModel from "@/models/movie"
-import to from "await-to-js"
 
 // In src/controllers/workoutController.js
 export const getAll = async (filter: any) => {
@@ -10,11 +9,8 @@ export const getAll = async (filter: any) => {
 }
 
 export const getOne = async (id: string) => {
-  const [error, movie] = await to(db.collection("movies").findOne({ _id: new ObjectId(id) }))
-  console.log(movie)
-  console.log(error)
-  // if (movie) return movie
-  // return error
+  const movie = await db.collection("movies").findOne({ _id: new ObjectId(id) })
+  return movie
 }
 
 export const createOne = async (req: any, res: any) => {
