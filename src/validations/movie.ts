1cfad1e725f62ae5242942765bc12f4c7df9ca29
lifeike <lifeike@gmail.com>
2023-08-08 01:24:31 -0600

    ad

--- src/validations/movie.ts
+++ src/validations/movie.ts
@@ -2,10 +2,7 @@ const Joi = require("joi")
 
 export const getAllMovies = {
   query: Joi.object().keys({
-    name: Joi.string(),
-    role: Joi.string(),
-    sortBy: Joi.string(),
-    limit: Joi.number().integer(),
-    page: Joi.number().integer(),
+    items_per_page: Joi.string(),
+    page_number: Joi.string(),
   }),
 }
