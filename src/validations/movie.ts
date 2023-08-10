967829a51138dca38a80094637ffa76dd342c5d0
lifeike <lifeike@gmail.com>
2023-08-10 16:31:10 -0600

    fix

--- src/validations/movie.ts
+++ src/validations/movie.ts
@@ -2,7 +2,7 @@ const Joi = require("joi")
 
 export const getAllMovies = {
   query: Joi.object().keys({
-    items_per_page: Joi.string(),
-    page_number: Joi.string(),
+    items_per_page: Joi.string().required(),
+    page_number: Joi.string().required(),
   }),
 }
