752626314abcd3fa1bed71e7f8b7c712e8c29891
lifeike <lifeike@gmail.com>
2023-08-10 13:33:13 -0600

    fix

--- src/config/db/mongoDB.ts
+++ src/config/db/mongoDB.ts
@@ -1,8 +1,7 @@
-// const { MongoClient } = require("mongodb")
-const config = require("@/config/config")
 import { MongoClient } from "mongodb"
+import config from "@/config/config"
 
-const client = new MongoClient(config.mongo.url)
+const client = new MongoClient(config?.mongo?.url)
 client.connect()
 console.log("database running.")
 const db = client.db("template")
