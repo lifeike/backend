f9a4e87ade07b20b00f347f5ba772df9155714c3
lifeike <lifeike@gmail.com>
2023-08-10 12:38:58 -0600

    ad

--- src/config/db/mongoDB.ts
+++ src/config/db/mongoDB.ts
@@ -1,10 +1,9 @@
 // const { MongoClient } = require("mongodb")
+const config = require("@/config/db/mongoDB")
 import { MongoClient } from "mongodb"
-const url = "mongodb+srv://feeco:Test%401234@cluster0.de2gw.mongodb.net/?retryWrites=true&w=majority"
-const client = new MongoClient(url)
+console.log(config.MONGODB_URL)
+const client = new MongoClient(config.MONGODB_URL)
 client.connect()
 console.log("database running.")
-
 const db = client.db("template")
-
 export default db
