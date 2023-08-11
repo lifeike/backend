39310fd8e9a4e6ebf740b522be79fb4f9544db42
lifeike <lifeike@gmail.com>
2023-08-11 09:30:27 -0600

    ad

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,11 @@
-const express = require("express")
+import express, { Request, Response } from "express"
 const router = express.Router()
-const path = require("path")
 
-router.get("/", (req, res) => res.send("Feeco's server is running."))
+router.get("/", (req: Request, res: Response) => res.send("Feeco's server is running."))
 router.use("/movies", require("./movie.ts"))
 // router.use("/auth", require("./auth"))
 // router.use("/chat", require("./chat"))
 // router.use("/user", require("./user"))
 // router.use("/upload", require("./upload"))
 
-module.exports = router
+export default router
