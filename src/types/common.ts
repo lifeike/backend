940f354c8d8e409e659e503771218458739e20a8
lifeike <lifeike@gmail.com>
2023-08-15 19:59:22 -0600

    fix

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,14 +1,10 @@
 import express, { Express, Request, Response } from "express"
-export type RequestParams = Request<
-  null,
-  null,
-  null,
-  {
-    search: string
-    role: string
-    status: string
-    sortBy: string
-    perPage: number
-    pageNo: number
-  }
->
+export type SearchParams = {
+  search: string
+  role: string
+  status: string
+  sortBy: string
+  perPage: number
+  pageNo: number
+}
+export type RequestParams = Request<null, null, null, SearchParams>
