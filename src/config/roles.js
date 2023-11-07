df9bad8d23a542c258ee8154b10d42c41312454c
lifeike <lifeike@gmail.com>
2023-11-06 20:46:10 -0700

    ad

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,12 +1,9 @@
 const allRoles = {
   user: [],
-  admin: ['getUsers', 'manageUsers'],
-};
+  admin: ["getUsers", "manageUsers"],
+}
 
-const roles = Object.keys(allRoles);
-const roleRights = new Map(Object.entries(allRoles));
+const roles = Object.keys(allRoles)
+const roleRights = new Map(Object.entries(allRoles))
 
-module.exports = {
-  roles,
-  roleRights,
-};
+export default { roles, roleRights }
