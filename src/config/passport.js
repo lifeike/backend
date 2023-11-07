fafb8aed37f8667675e7f061d8400505c2159a0a
lifeike <lifeike@gmail.com>
2023-11-06 20:45:33 -0700

    ad

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,30 +1,26 @@
-const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
-const config = require('./config');
-const { tokenTypes } = require('./tokens');
-const { User } = require('../models');
+import { Strategy: JwtStrategy, ExtractJwt } from "passport-jwt"
+import config from "./config"
+import { tokenTypes } from "./tokens"
+import { User } from "../models"
 
 const jwtOptions = {
   secretOrKey: config.jwt.secret,
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-};
+}
 
-const jwtVerify = async (payload, done) => {
+export const jwtVerify = async (payload, done) => {
   try {
     if (payload.type !== tokenTypes.ACCESS) {
-      throw new Error('Invalid token type');
+      throw new Error("Invalid token type")
     }
-    const user = await User.findById(payload.sub);
+    const user = await User.findById(payload.sub)
     if (!user) {
-      return done(null, false);
+      return done(null, false)
     }
-    done(null, user);
+    done(null, user)
   } catch (error) {
-    done(error, false);
+    done(error, false)
   }
-};
+}
 
-const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
-
-module.exports = {
-  jwtStrategy,
-};
+const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify)
